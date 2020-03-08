53be96aee949c4f8349dde713542fa7b67d30bc3
gundrm <rachelm.gundy@gmail.com>
2020-03-08 15:48:00 -0700

    header update

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import Carousel from '../components/Carousel';
 
-function ProjectsPage(props) {
+function ProjectsPage() {
 
     return(
+        <div>
+            <Container fluid={true}>
+            <Row className="justify-content-left py-2">
+                <Col md={8} sm={12}>
+                    <h1 className="display-1 font-weight-bolder">Personal Projects</h1>
+                    <h3 className="display-5 font-weight-light">Click on Projects for Additional Details:</h3>
+                </Col>
+            </Row>
+        </Container>
 
         <Carousel />
+        </div>
+        
 
     );
 }
